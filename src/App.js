5c42532daf8addf09b7707351e35d9c9366ff3cb
mmbits <itsallbits@gmail.com>
2019-07-22 10:03:05 -0700

    ready for new week continuing with new app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    memes:[],
+    loading:false,
+    text:'',
+  }
+
+  getMemes = async (e) => {  //async allows use of await
+    e.preventDefault()  //prevents page reloading
+    this.setState({loading:true})
+  }
+
+  render() {
+    var {memes, loading, text} = this.state
+    return (
+      <div className="App">
+        stuff goes here
+        <form className="App-header" onSubmit={this.getMemes}>
+          <input value={text}
+            onChange={e=> this.setState({text: e.target.value})}
+          />
+          <button disabled={loading || !text} type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
